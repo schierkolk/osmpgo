4828ac02ec63856cd9fe91ba994c6b73fed703bf
Chris Schierkolk <thebengrimm@gmail.com>
2021-01-21 15:52:25 -0500

    Update timeit4me.py
    20

--- timeit4me.py
+++ timeit4me.py
@@ -21,4 +21,4 @@ def test():
 
 if __name__ == '__main__':
     import timeit
-    print(timeit.timeit("test()", setup="from __main__ import test", number=10))
+    print(timeit.timeit("test()", setup="from __main__ import test", number=20))
